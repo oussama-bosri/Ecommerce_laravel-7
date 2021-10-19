24aedde1c6b31fa21ee2465d88abc26f8d60ae7c
OUSSAMABOSRI <bosrioussama@gmail.com>
2021-10-19 10:19:57 +0100

    admin sidebar

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-4">
+            @include("layouts.sidebar")
         </div>
         <div class="col-md-8">   
             <table class="table table-hover">
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,15 @@
+<ul class="list-group mb-3">
+                <a href="{{ route("admin.products") }}" class="list-group-item 
+                    font-weight-bold 
+                    list-group-item-action">
+                    <i class="fa fa-list"></i>
+                    Produits
+                </a>
+                <a href="{{ route("admin.orders") }}" class="list-group-item 
+                    font-weight-bold 
+                    list-group-item-action">
+                    <i class="fa fa-credit-card"></i>
+                    Commandes
+                </a>
+
+</ul>
